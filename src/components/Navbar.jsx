1c05304df0620677d736a41fc7c6e6e7afb02240
Sayak Saha <157394372+SayakSaha2002@users.noreply.github.com>
2025-04-18 21:17:09 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -22,11 +22,7 @@ const Navbar = () => {
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          {/* <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="#">Home</a>
-            </li>
-          </ul> */}
+          
           <form className="d-flex" onSubmit={handleSubmit}>
             <input
               className="form-control me-2"
